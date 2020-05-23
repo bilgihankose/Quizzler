5ceb3cc2c82fbc55a0c38cf1dbd60e969ae8d09d
Bilgihan Kose <bilgihankose@gmail.com>
2020-05-23 12:28:06 +0300

    upload github

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Bilgihan Köse on 23.05.2020.
+//  Copyright © 2020 Bilgihan Kose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
